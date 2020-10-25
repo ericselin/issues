13f6265dcac44f2f4eca8cf8fd5119bdea80d85b
Eric Selin <eric.selin@gmail.com>
2020-10-25 10:00:03 +0000

    feat: Add argv parsing and httpoison package

---
+++ lib/issues/cli.ex
@@ -0,0 +1,52 @@
+defmodule Issues.CLI do
+  @default_count 4
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_domain
+  end
+
+  defp args_to_domain([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  defp args_to_domain([user, project]) do
+    {user, project, @default_count}
+  end
+
+  defp args_to_domain(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+end
